bd1f7af771f48fe89096b43e0c74f92b754b8e9a
danielfaria <danielcordeirofaria@gmail.com>
2022-05-15 00:32:43 -0300

    atualização do dia

---
+++ index.js
@@ -0,0 +1,5 @@
+var palavras = ["palavras", "computador" , "programaçao" , "hoje" , "celular" , "hospital" , "bomba"];
+var botaoComecar = document.querySelector(".botaoComecar")
+botaoComecar.addEventListener("click", function(){
+    console.log("palavras");
+});
