ee91cfd775da71d5dde1c73cc37eea79f24db008
danielfaria <danielcordeirofaria@gmail.com>
2022-05-20 14:15:02 -0300

    addNovaPalavra

--- adicionarUmaNovaPalavra.js
+++ adicionarUmaNovaPalavra.js
@@ -1,5 +1,13 @@
-const palavrasExistentes = localStorage.getItem('palavras');
-var adicionarUmaNovaPalavra = document.getElementById(adicionarUmaNovaPalavra);
-var salvarEComeçar = adicionarUmaNovaPalavra.addEventListener("click", )
-let novasPalavras = palavrasExistentes.push(palavraAdicionada);
-console.log(novasPalavras)
+function addNovaPalavra{
+    var selecionandoTextArea = document.getElementById("palavraAdicionada").value;
+    var novasPalavras = palavrasExistentes.push("selecionandoTextArea");
+    return novasPalavras
+};
+let palavrasExistentes = localStorage.getItem("palavras");
+
+var adicionarUmaNovaPalavra = document.querySelector("salvarEComecar");
+
+var salvarEComeçar = adicionarUmaNovaPalavra.addEventListener("click", addNovaPalavra());
+
+console.log(salvarEComeçar);
+
