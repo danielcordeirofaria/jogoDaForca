bb945a83694078db16af767117edcdde119e880f
danielcordeirofaria <danielcordeirofaria@gmail.com>
2022-05-26 15:54:05 -0300

    for para confirmação

--- jogoDaForca.js
+++ jogoDaForca.js
@@ -1,32 +1,30 @@
 
 
 let palavrasExistentes = localStorage.getItem("palavras");
-console.log(palavras)
 var sorteidoDaPalavra = Math.floor(Math.random() * palavras.length);
 var palavraSorteada = palavras[sorteidoDaPalavra]
-console.log(palavraSorteada);
 var palavraPreparada = palavraSorteada.split("", palavraSorteada.value);
 console.log(palavraPreparada);
 
 for (var i = 0; i < palavraPreparada.length; i++) {
     var divParaPalavra = document.querySelector(".divParaPalavra")
-    console.log(divParaPalavra)
     var div1 = document.createElement("div");
     div1.classList.add("cadaLetra");
     divParaPalavra.appendChild(div1);
     div1.textContent = palavraPreparada[i];
 };
 
+var selecaoDaTeclaA = document.querySelector(".a");
 
-
-var selecaoDaLetraA = document.querySelector(".a");
-var letraA = selecaoDaLetraA.addEventListener("click", verificacaoDeLetraA)
-
-function verificacaoDeLetraA (){
-        if(a == palavraPreparada)
-            var confirmandoA = document.querySelector("palavraPreparada");
-            confirmandoA.classList.replace("cadaLetra", "letraAcertada");
-
-}
-
-
+var letraA = selecaoDaTeclaA.addEventListener("click", function() {
+    for(i = 0; palavraPreparada.length < i; i++){
+        if(palavraPreparada[i].includes("A")){
+            var confirmandoA = document.querySelector(".cadaLetra");
+            confirmandoA.classList.remove("cadaLetra");
+            confirmandoA.classList.add("letraAcertada");
+            console.log(confirmandoA.value);
+        } else {
+            var removendoA = document.querySelector(".a").classList.replace(".letraErrada")
+        }
+    }
+});
