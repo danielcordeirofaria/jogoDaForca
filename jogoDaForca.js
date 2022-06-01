365139681fe74dea3cb7f471b2c946880db9306e
danielfaria <danielcordeirofaria@gmail.com>
2022-06-01 15:52:42 -0300

    novas palavras

--- jogoDaForca.js
+++ jogoDaForca.js
@@ -1,32 +1,47 @@
+var palavraPreparada = [];
+
+
+
+//function sorteandoPalavra(){
+    let palavrasExistentes = localStorage.getItem("palavras");
+    var sorteidoDaPalavra = Math.floor(Math.random() * palavras.length);
+    var palavraSorteada = palavras[sorteidoDaPalavra];
+    var palavraPreparada = palavraSorteada.split("", palavraSorteada.value);
+    console.log(palavraPreparada);
+    //return palavraPreparada
+//};
+
+//function criandoElementos(){
+    for (var i = 0; i < palavraPreparada.length; i++) {
+        var divParaPalavra = document.querySelector(".divParaPalavra")
+        var div1 = document.createElement("div");
+        div1.classList.add("cadaLetra");
+        divParaPalavra.appendChild(div1);
+        div1.textContent = palavraPreparada[i];
+    };
+//};
+
 function confirmandoLetraA(){
-    var selecionandoClasse = document.querySelectorAll(".cadaLetra");
-    var valorSelecionandoClasse = selecionandoClasse.value;
-    console.log(valorSelecionandoClasse);
-    if (valorSelecionandoClasse.contains(A)) {
-        selecionandoClasse.classList.replace("cadaLetra", "letraEncontrada");
-    } else {
-        var valorSelecionandoClasse = document.querySelector(".cadaLetra");
-        selecionandoClasse.classList.replace("cadaLetra", "letraErrada");
-    }
+    //for(i = 0; palavraPreparada.length <= i ; i++){
+    var existeLetra = palavraPreparada.includes("a")
+        if( existeLetra == true){
+            console.log("deucerto");
+            var localizacaoDaLetra = palavraPreparada.indexOf("a");
+            console.log(localizacaoDaLetra);
+        } else {
+            console.log("não possui a letra")
+        }
 };
 
 
-let palavrasExistentes = localStorage.getItem("palavras");
-var sorteidoDaPalavra = Math.floor(Math.random() * palavras.length);
-var palavraSorteada = palavras[sorteidoDaPalavra]
-var palavraPreparada = palavraSorteada.split("", palavraSorteada.value);
-console.log(palavraPreparada);
+//sorteandoPalavra();
+//criandoElementos();
+
+//Verificar mediante um array do alfabeto. com textcontent e indexOf
 
 
-for (var i = 0; i < palavraPreparada.length; i++) {
-    var divParaPalavra = document.querySelector(".divParaPalavra")
-    var div1 = document.createElement("div");
-    div1.classList.add("cadaLetra");
-    divParaPalavra.appendChild(div1);
-    div1.textContent = palavraPreparada[i];
-};
 
-var selecionandoBotaoLetraA = document.getElementById("letra");
+var selecionandoBotaoLetraA = document.querySelector(".a");
 
-var clickandoNaLetraA = selecionandoBotaoLetraA.addEventListener("click", confirmandoLetraA);
+selecionandoBotaoLetraA.addEventListener("click", confirmandoLetraA);
 
